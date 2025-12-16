bb5497b4254b6147a03032a3ae5a98da265e6b8a
Peng Lu <150557980+PNGL9527@users.noreply.github.com>
2025-12-16 08:49:05 +0100

    Update astro.config.mjs 加回 i18n

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import tailwindcss from '@tailwindcss/vite';
 import icon from 'astro-icon';
-//import { languages, prefixDefaultLocale, defaultLocale } from './src/i18n/i18n.ts';
+import { languages, prefixDefaultLocale, defaultLocale } from './src/i18n/i18n.ts';
 
 // https://astro.build/config
 export default defineConfig({
